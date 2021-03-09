8c4014efec287f1eb8f917d55fb619e9a2e5b874
Nguyen Ngoc Hung <69026717+Nguyen-Ngoc-Hung@users.noreply.github.com>
2021-03-09 21:09:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var yearBirth;
+
+function age()
+{
+    yearBirth = prompt('Your year birth:')
+    var yourAge = 2021 -yearBirth;
+    console.log('Your age: ' + yourAge);
+    var h1 = document.createElement('h1');
+    var answer = document.createTextNode('Your age: ' + yourAge);
+    h1.setAttribute("id","out-put");
+    h1.appendChild(answer);
+    document.getElementById('result-box').appendChild(h1);
+}
